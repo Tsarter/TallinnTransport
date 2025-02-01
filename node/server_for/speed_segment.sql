b53bcc48f6ece9aa2ae929924a75c7dcd6c889d6
tatall <tatall@ttu.ee>
2025-02-01 16:43:28 +0200

    Tiny fix

--- node/server_for/speed_segment.sql
+++ node/server_for/speed_segment.sql
@@ -13,7 +13,7 @@ segment_data AS (
     SELECT 
         line,
         vehicle_id,
-        ST_MakeLine(point::GEOMETRY, next_point::GEOMETRY) AS segment, -- Create a segment (LINESTRING) for each pair of points
+        ST_AsGeoJSON(ST_MakeLine(point::GEOMETRY, next_point::GEOMETRY)) AS segment, -- Create a segment (LINESTRING) for each pair of points
         EXTRACT(EPOCH FROM (next_time - datetime)) AS time_diff_seconds, -- Time difference in seconds
         ST_Distance(point::GEOGRAPHY, next_point::GEOGRAPHY) AS distance_meters, -- Distance in meters
         CASE 
