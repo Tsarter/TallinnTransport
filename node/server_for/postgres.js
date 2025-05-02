177e202dbf3feb53e82e1713f734d713eea41c69
tatall <tatall@ttu.ee>
2025-05-02 20:33:41 +0300

    Refactor validation and query handling in speed segments and location endpoints; improve code readability and consistency

--- node/server_for/postgres.js
+++ node/server_for/postgres.js
@@ -11,7 +11,7 @@ const { Pool } = require("pg");
 const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
-require("dotenv").config()
+require("dotenv").config();
 
 // Create an Express application
 const app = express();
@@ -28,13 +28,30 @@ const pool = new Pool({
 });
 
 function getQuery(type, filename) {
-  return fs.readFileSync(path.join(__dirname+"/sql/" + type + "/", filename), "utf8");
+  return fs.readFileSync(
+    path.join(__dirname + "/sql/" + type + "/", filename),
+    "utf8"
+  );
 }
-  
+
 function validateParams(query) {
-  const { type, line, date, startHour, tws,maxSpeed, disStops, vehicle_id, datetime,
-    lat1, lon1, lat2, lon2} = query;
-  
+  const {
+    type,
+    line,
+    date,
+    startTime,
+    tws,
+    maxSpeed,
+    disStops,
+    vehicle_id,
+    endtime,
+    lat1,
+    lon1,
+    lat2,
+    lon2,
+    datetime,
+  } = query;
+
   // Validate that date is in YYYY-MM-DD
   if (date && !/^\d{4}-\d{2}-\d{2}$/.test(date)) {
     return "Date must be in the format YYYY-MM-DD";
@@ -42,38 +59,47 @@ function validateParams(query) {
   let inputDate = new Date(date);
   let startDate = new Date("2024-06-06");
   let today = new Date();
-  
+
   if (inputDate < startDate || inputDate > today) {
     return "Date must be between 6 june 2024 and today";
   }
 
-  if (datetime && !/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/.test(datetime)) {
-    return "datetime must be in the format YYYY-MM-DD HH:MM:SS";
+  for (const timestamp of [startTime, endtime, datetime]) {
+    if (timestamp && !/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/.test(timestamp)) {
+      return "Timestamp must be in the format YYYY-MM-DD HH:MM:SS";
+    }
   }
 
   // Validate that startHour and endHour are integers
-  if (startHour && (!Number.isInteger(Number(startHour)) || !Number.isInteger(Number(tws)) || Number(tws) > 124)) {
+  if (
+    startHour &&
+    (!Number.isInteger(Number(startHour)) ||
+      !Number.isInteger(Number(tws)) ||
+      Number(tws) > 124)
+  ) {
     return "startHour and endHour must be integers";
   }
-  
+
   // Validate that lineparam is like this 1,2,3,10,18,84,18A,47B
   const lineRegex = /^(?:\d{1,3}[A-Z]?)$/;
   if (line && !lineRegex.test(line)) {
     return "Line must be a number between 1 and 999 or a number followed by a letter";
   }
-  
+
   // Validate that maxSpeed is an integer
   if (maxSpeed && !Number.isInteger(Number(maxSpeed))) {
     return "maxSpeed must be an integer";
-
   }
 
-  if (type && !["","1","2","3"].includes(type)) {
+  if (type && !["", "1", "2", "3"].includes(type)) {
     return "Type must be empty, 1, 2 or 3";
   }
 
-  if (disStops && (!Number.isInteger(Number(disStops)) || Number(disStops) > 1000)) {
-    return "filter stops is wrong"
+  if (
+    disStops &&
+    (!Number.isInteger(Number(disStops)) || Number(disStops) > 1000)
+  ) {
+    return "filter stops is wrong";
   }
 
   if (vehicle_id && !Number.isInteger(Number(vehicle_id))) {
@@ -83,73 +109,65 @@ function validateParams(query) {
   // longitude and latitude must be in the format 1.234567
   for (const location in [lat1, lon1, lat2, lon2]) {
     if (location && !/^-?\d+(\.\d+)?$/.test(location)) {
-    return "Location is wrong";
-  }}
-
+      return "Location is wrong";
+    }
+  }
 
   return "";
 }
 
 // Define a GET endpoint to query the table
 app.get("/speedsegments", async (req, res) => {
-  try{
-  console.log(req.query);
-  const isValidRes = validateParams(req.query);
-  if (isValidRes !== "") {
-    console.log(isValidRes);
-    return res.status(400).json({ error: isValidRes });
-  }
-
-  const { type, line, datetime, startHour, tws,maxSpeed, disStops } = req.query;
-
-  if (!datetime || !startHour || !tws) {
-    return res.status(400).send("Date, startHour, and endHour are required");
-  }
-
   try {
+    console.log(req.query);
+    const isValidRes = validateParams(req.query);
+    if (isValidRes !== "") {
+      console.log(isValidRes);
+      return res.status(400).json({ error: isValidRes });
+    }
 
-    // Put together query string
-    let speed_data = getQuery("segment", "speed_data.sql");
-    let segment_data =  getQuery("segment","segment_data.sql");
-    let select_data =  getQuery("segment","select_data.sql");
-    let not_within_stop =  getQuery("segment","not_within_stop.sql");
+    const { type, line, startTime, endTime, maxSpeed, disStops } = req.query;
 
-    const startTime = `${datetime}`;
-    
-    let endTime = new Date(startTime);
-    endTime.setHours(endTime.getHours() + parseInt(tws));
-    endTime.setMinutes(endTime.getMinutes() - endTime.getTimezoneOffset());
-    endTime = endTime.toISOString().slice(0, 19).replace("T", " ");
-    // Add the last line dynamically
-    speed_data += `datetime BETWEEN '${startTime}' AND  '${endTime}' `;
-    speed_data += line ? ` AND line = '${line}' ` : '';
-    speed_data += type ? ` AND type = ${type} ` : '';
-    speed_data += `AND NOT EXISTS (
+    if (!startTime || !endTime) {
+      return res.status(400).send("Missing required params");
+    }
+
+    try {
+      // Put together query string
+      let speed_data = getQuery("segment", "speed_data.sql");
+      let segment_data = getQuery("segment", "segment_data.sql");
+      let select_data = getQuery("segment", "select_data.sql");
+      let not_within_stop = getQuery("segment", "not_within_stop.sql");
+
+      // Add the last line dynamically
+      speed_data += `datetime BETWEEN '${startTime}' AND  '${endTime}' `;
+      speed_data += line ? ` AND line = '${line}' ` : "";
+      speed_data += type ? ` AND type = ${type} ` : "";
+      speed_data += `AND NOT EXISTS (
       SELECT 1
       FROM depos
       WHERE ST_Within(geom::geometry, depos.location::geometry)
   )`;
 
-    select_data += maxSpeed ? ` AND speed_kmh < ${maxSpeed}` : '';
+      select_data += maxSpeed ? ` AND speed_kmh < ${maxSpeed}` : "";
 
-    let query = "";
-    if (disStops) {
-      not_within_stop += disStops ? ` ${disStops}` : '';
-      query = `${speed_data}), ${segment_data} ${select_data} ${not_within_stop}));`;
-    }
-    else{
-      // Combine all parts of the query
-      query = `${speed_data}), ${segment_data} ${select_data};`;
-    }
+      let query = "";
+      if (disStops) {
+        not_within_stop += disStops ? ` ${disStops}` : "";
+        query = `${speed_data}), ${segment_data} ${select_data} ${not_within_stop}));`;
+      } else {
+        // Combine all parts of the query
+        query = `${speed_data}), ${segment_data} ${select_data};`;
+      }
 
-    //console.log(query);
-    // Execute the query
-    const result = await pool.query(query);
-    res.json(result.rows);
-  } catch (err) {
-    console.error("Error querying the database:", err);
-    res.status(500).send("Internal Server Error");
-  }
+      //console.log(query);
+      // Execute the query
+      const result = await pool.query(query);
+      res.json(result.rows);
+    } catch (err) {
+      console.error("Error querying the database:", err);
+      res.status(500).send("Internal Server Error");
+    }
   } catch (err) {
     console.error("Error querying the database:", err);
     res.status(500).send("Internal Server Error");
@@ -157,7 +175,6 @@ app.get("/speedsegments", async (req, res) => {
 });
 
 app.get("/speedgraph", async (req, res) => {
-  
   const isValidRes = validateParams(req.query);
   if (isValidRes !== "") {
     console.log(isValidRes);
@@ -170,7 +187,7 @@ app.get("/speedgraph", async (req, res) => {
   if (!tws) {
     tws = 15;
   }
-  try{
+  try {
     let endTime = new Date(startTime);
     endTime.setHours(endTime.getHours() + parseInt(tws));
     endTime.setMinutes(endTime.getMinutes() - endTime.getTimezoneOffset());
@@ -190,14 +207,13 @@ app.get("/speedgraph", async (req, res) => {
     console.log(query);
     const result = await pool.query(query);
     res.json(result.rows);
-  }catch (err) {
+  } catch (err) {
     console.error("Error querying the database:", err);
     res.status(500).send("Internal Server Error");
   }
 });
 
 app.get("/gridspeeds", async (req, res) => {
-
   const isValidRes = validateParams(req.query);
   if (isValidRes !== "") {
     console.log(isValidRes);
@@ -210,7 +226,7 @@ app.get("/gridspeeds", async (req, res) => {
   if (!tws) {
     tws = 15;
   } */
-  try{
+  try {
     /* let endTime = new Date(startTime);
     endTime.setHours(endTime.getHours() + parseInt(tws));
     endTime.setMinutes(endTime.getMinutes() - endTime.getTimezoneOffset());
@@ -222,27 +238,25 @@ app.get("/gridspeeds", async (req, res) => {
     console.log(query);
     const result = await pool.query(query);
     res.json(result.rows);
-  }catch (err) {
+  } catch (err) {
     console.error("Error querying the database:", err);
     res.status(500).send("Internal Server Error");
   }
-}
-);
+});
 app.get("/stops", async (req, res) => {
-  try{
+  try {
     let select = getQuery("stops", "stops.sql");
     const query = `${select}`;
     const result = await pool.query(query);
     res.json(result.rows);
-  }catch (err) {
+  } catch (err) {
     console.error("Error querying the database:", err);
     res.status(500).send("Internal Server Error");
   }
 });
 
-
 app.get("/loc_to_loc", async (req, res) => {
-  try{
+  try {
     const isValidRes = validateParams(req.query);
     if (isValidRes !== "") {
       console.log(isValidRes);
@@ -257,18 +271,17 @@ app.get("/loc_to_loc", async (req, res) => {
       ),
       point_b AS (
         SELECT ST_SetSRID(ST_MakePoint(${lon2},${lat2}), 4326)::geography AS geom
-      )`
+      )`;
     let select = getQuery("loc_to_loc", "loc_to_loc.sql");
     const query = `${points} ${select}`;
     console.log(query);
     const result = await pool.query(query);
     res.json(result.rows);
-  }catch (err) {
+  } catch (err) {
     console.error("Error querying the database:", err);
     res.status(500).send("Internal Server Error");
   }
 });
-
 
 // Start the server
 app.listen(port, () => {
