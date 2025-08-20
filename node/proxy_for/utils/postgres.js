3dc209b6e9c5794202eccdef7478e62cbc280a74
Tanel <TalloTanel@gmail.com>
2025-08-20 12:40:34 +0300

    feat: add PostgreSQL database connection utility and Express server setup

---
+++ node/proxy_for/utils/postgres.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+require("dotenv").config({ path: path.join(__dirname, "../../.env") });
+
+const knex = require("knex");
+
+const db = knex({
+  client: "postgresql",
+  connection: {
+    host: process.env.POSTGRES_HOST || "127.0.0.1",
+    port: process.env.POSTGRES_PORT || 5432,
+    user: process.env.PG_TANEL_USER,
+    password: process.env.PG_TANEL_PASSWORD,
+    database: process.env.TRANSPORT_DB,
+  },
+  pool: { min: 2, max: 10 },
+});
+
+module.exports = db;
